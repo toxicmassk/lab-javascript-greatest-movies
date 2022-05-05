2c3a2c19ec3f8400d5277b3f526d6c37cc96413a
Julia Lipniewicz <julialipniewicz@yahoo.de>
2022-05-05 14:36:15 +0200

    first commit

--- src/movies.js
+++ src/movies.js
@@ -6,7 +6,7 @@ console.log('movies: ', movies);
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors() {}
-
+console.log('Hello');
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies() {}
 
