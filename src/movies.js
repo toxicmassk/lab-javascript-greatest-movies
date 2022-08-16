4247e599c4bf5250389402ef1f2940b7210cd86e
Julia Lipniewicz <julialipniewicz@yahoo.de>
2022-08-16 15:57:44 +0200

    tried

--- src/movies.js
+++ src/movies.js
@@ -59,30 +59,36 @@
   },
  ];
 
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(movies) {
-const directorsArr = movies.map(movie => movie.director)
-return directorsArr
-} 
+function getAllDirectors(moviesArr) {
+  return moviesArr
+    .map(function (movie) {
+      return movie.director;
+    })
+  };
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(movies) {
- let filterMovies = movies.filter(function(movie){
-   if (movie.director === 'Steven Spielberg' && movie.genre.includes ('Drama')){
- return true
-   }
- })
- return filterMovies.length
-};
+function howManyMovies(moviesArr) {
+     return moviesArr
+     .filter(function (movie) {
+       return movie.director === "Steven Spielberg"
+     })
+     .filter(function (movie) {
+       return movie.genre.includes("Drama")
+     }).length;
+}
+
+
 
 
 //howManyMovies(testArr);
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(movies) {
-  if(movies.length === 0){
+function scoresAverage() {}
+  /*if(movies.length === 0){
     return 0
   }
   let sumScore = movies.reduce((accumulator, currentElement) => {
@@ -93,22 +99,22 @@ function scoresAverage(movies) {
   }
  },0)
 return Number((sumScore/ movies.length).toFixed(2))
-}
+} */
 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(movies) {
+function dramaMoviesScore(){}
+/*function dramaMoviesScore(movies) {
   let sumDrama = movies.filter(function(movie){
     return movie.genre.includes ('Drama')
   })
   averageScore = scoresAverage(sumDrama)
   return averageScore
-};
+};*/
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(movies) {
-  let 
-}
+function orderByYear() {}
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
